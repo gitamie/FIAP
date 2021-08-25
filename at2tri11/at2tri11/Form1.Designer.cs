dffd7e66af76c29fa6518b5f4f20bd7ff46037a2
gitamie <86110056+gitamie@users.noreply.github.com>
2021-08-24 21:06:46 -0300

    Add files via upload

---
+++ at2tri11/at2tri11/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace at2tri11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.txtpeso = new System.Windows.Forms.TextBox();
+            this.txtaltura = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(88, 149);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(64, 35);
+            this.btncalcular.TabIndex = 0;
+            this.btncalcular.Text = "calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click_1);
+            // 
+            // txtpeso
+            // 
+            this.txtpeso.Location = new System.Drawing.Point(74, 53);
+            this.txtpeso.Name = "txtpeso";
+            this.txtpeso.Size = new System.Drawing.Size(100, 20);
+            this.txtpeso.TabIndex = 1;
+            // 
+            // txtaltura
+            // 
+            this.txtaltura.Location = new System.Drawing.Point(74, 102);
+            this.txtaltura.Name = "txtaltura";
+            this.txtaltura.Size = new System.Drawing.Size(100, 20);
+            this.txtaltura.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "peso";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "altura";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(272, 253);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtaltura);
+            this.Controls.Add(this.txtpeso);
+            this.Controls.Add(this.btncalcular);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.TextBox txtpeso;
+        private System.Windows.Forms.TextBox txtaltura;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ at2tri11/at2tri11/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace at2tri11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            double peso, altura;
+
+            peso = Convert.ToDouble(txtpeso.Text);
+            altura = Convert.ToDouble(txtaltura.Text);
+
+            if (altura >= 4.00 && altura <= 4.50)
+            {
+                MessageBox.Show("Você pode passar pela ponte A");
+            }
+
+            else
+            {
+                MessageBox.Show("Você ultrapassou a altura permitida e não pode passar pela ponte A");
+            }
+
+            if (altura >= 3.00 && altura <= 3.50)
+            {
+                MessageBox.Show("Você pode passar pela ponte B");
+            }
+
+            else
+            {
+                MessageBox.Show("Você ultrapassou a altura permitida e não pode passar pela ponte B");
+            }
+
+            if (altura >= 5.00 && altura <= 5.50)
+            {
+                MessageBox.Show("Você pode passar pela ponte C");
+            }
+
+            else
+            {
+                MessageBox.Show("Você ultrapassou a altura permitida e não pode passar pela ponte C");
+
+
+                if (peso >= 300 && peso <= 350)
+                {
+                    MessageBox.Show("Você pode passar pela ponte A");
+                }
+
+                else
+                {
+                    MessageBox.Show("Você ultrapassou o peso permitido e não pode passar pela ponte A");
+                }
+
+                if (peso >= 400 && peso <= 450)
+                {
+                    MessageBox.Show("Você pode passar pela ponte B");
+                }
+
+                else
+                {
+                    MessageBox.Show("Você ultrapassou o peso permitido e não pode passar pela ponte B");
+                }
+
+                if (peso >= 500 && peso <= 550)
+                {
+                    MessageBox.Show("Você pode passar pela ponte C");
+                }
+
+                else
+                {
+                    MessageBox.Show("Você ultrapassou o peso permitido e não pode passar pela ponte C");
+
+
+                }
+
+
+
+
+
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncalcular_Click_1(object sender, EventArgs e)
+        {
+            double peso, altura;
+
+            peso = Convert.ToDouble(txtpeso.Text);
+            altura = Convert.ToDouble(txtaltura.Text);
+
+            if (altura >= 4.00 && altura <= 4.50)
+            {
+                MessageBox.Show("Você pode passar pela ponte A");
+            }
+
+            else
+            {
+                MessageBox.Show("Você ultrapassou a altura permitida e não pode passar pela ponte A");
+            }
+
+            if (altura >= 3.00 && altura <= 3.50)
+            {
+                MessageBox.Show("Você pode passar pela ponte B");
+            }
+
+            else
+            {
+                MessageBox.Show("Você ultrapassou a altura permitida e não pode passar pela ponte B");
+            }
+
+            if (altura >= 5.00 && altura <= 5.50)
+            {
+                MessageBox.Show("Você pode passar pela ponte C");
+            }
+
+            else
+            {
+                MessageBox.Show("Você ultrapassou a altura permitida e não pode passar pela ponte C");
+
+
+                if (peso >= 300 && peso <= 350)
+                {
+                    MessageBox.Show("Você pode passar pela ponte A");
+                }
+
+                else
+                {
+                    MessageBox.Show("Você ultrapassou o peso permitido e não pode passar pela ponte A");
+                }
+
+                if (peso >= 400 && peso <= 450)
+                {
+                    MessageBox.Show("Você pode passar pela ponte B");
+                }
+
+                else
+                {
+                    MessageBox.Show("Você ultrapassou o peso permitido e não pode passar pela ponte B");
+                }
+
+                if (peso >= 500 && peso <= 550)
+                {
+                    MessageBox.Show("Você pode passar pela ponte C");
+                }
+
+                else
+                {
+                    MessageBox.Show("Você ultrapassou o peso permitido e não pode passar pela ponte C");
+
+
+                }
+            }
+        }
+    }
+}
+
+    
